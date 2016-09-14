591a2d22298f568c1d79ebdb500f23b71734f0a1
Raycent <raycent.tan@flatironschool.com>
2016-09-14 11:35:01 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,10 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
@@ -12,7 +12,7 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
@@ -20,11 +20,10 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
+  name = "Kristin";
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
   }
 }
